320bddd1c04c6f1d796641325054dce0318bea4e
emgreen33 <greenemily33@gmail.com>
2016-04-21 09:33:14 -0700

    Refactoring triangle type and weird string and creating new questions

--- triangletype.rb
+++ triangletype.rb
@@ -18,15 +18,3 @@ puts triangle_type(8,5,7)#, 1)  # Acute
 puts triangle_type(3,4,5)#, 2)  # Right
 puts triangle_type(7,12,8)#, 3) # Obtuse
 
-def list(names)
-  return '' if names.empty?
-  
-  names = names.map do |names|
-     names[:name]
-  end
-  if names.one? 
-     "#{names.first}"
-  else
-    "#{names[0...-1].join(', ')} & #{names.last}" 
-  end
-end
